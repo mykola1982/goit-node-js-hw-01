306d8e7549760dc6deb9648c90a2076aa33e3880
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-02-18 16:16:03 +0200

    fix

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function invokeAction({ action, id, name, email, phone }) {
       break;
 
     case "add":
-      const newContact = contacts.addContact(name, email, phone);
+      const newContact = await contacts.addContact(name, email, phone);
       console.log(newContact);
       break;
 
